39b0de88c044275b7a1bd8c2ec8aeb2a4d01a93e
badaev52 <badaev52@gmail.com>
2022-12-13 20:39:37 +0300

    task 43 solved

--- exercise41/Program.cs
+++ exercise41/Program.cs
@@ -4,5 +4,55 @@
 // 1, -7, 567, 89, 223-> 3
 
 
+int nums = inputInt("Задайте количество чисел ");
 
+int[] inpNumb = CountQuantityPositiveNumbers(nums, out int posNumb);
+PrintArray(inpNumb);
+Console.WriteLine($"Количество положительных чисел: {posNumb}");
 
+int[] CountQuantityPositiveNumbers(int count, out int res)
+{
+    res = 0;
+    int[] inputNumbers = new int[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        string str = $"Введите число {i + 1} из {count}";
+        int temp = inputInt(str);
+        inputNumbers[i] = temp;
+
+        if (temp > 0) res++;
+
+    }
+
+    return inputNumbers;
+}
+
+int inputInt(string str)
+{
+    Console.WriteLine(str + ": ");
+    bool isInt = int.TryParse(Console.ReadLine(), out int num);
+    if (isInt)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+        return -1;
+    }
+
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("Вы ввели: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        Console.Write(arr[i]);
+        if (i == arr.Length - 1) break;
+        Console.Write(", ");
+    }
+    Console.WriteLine();
+}
