0d75ba96d381c105868170e522aaae09cbbdb341
badaev52pk1 <badaev52@gmail.com>
2022-12-20 21:06:33 +0300

    Задача 64 решена

---
+++ exercise64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int N = 8;
+PrintNaturalNumberReqursive(N);
+
+void PrintNaturalNumberReqursive(int N){
+
+    int num0 = 1;
+
+    if(N > num0){
+        Console.Write($"{N}, ");
+        PrintNaturalNumberReqursive(N - 1);
+    }else if(N == num0){
+        Console.Write(num0);
+    }
+
+
+}
