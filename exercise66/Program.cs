02425aa84cfb86e6f4dac113f4fca4562df36d26
badaev52pk1 <badaev52@gmail.com>
2022-12-20 21:35:31 +0300

    задача 66 решена

--- exercise66/Program.cs
+++ exercise66/Program.cs
@@ -5,3 +5,37 @@
 // M = 4; N = 8. -> 30
 
 
+int numsM = inputInt("Задайте число М");
+int numsN = inputInt("Задайте число N");
+Console.WriteLine($" -> {PrintNaturalNumberReqursive(numsM, numsN)}");
+
+int PrintNaturalNumberReqursive(int interval0, int interval1)
+{
+
+    if (interval0 > interval1)
+    {
+        int temp = interval0;
+        interval0 = interval1;
+        interval1 = temp;
+    }
+
+    return (interval0 == interval1) ? interval1 : interval0 + PrintNaturalNumberReqursive(interval0 + 1, interval1);
+
+
+}
+
+int inputInt(string str)
+{
+    Console.WriteLine(str + ": ");
+    bool isInt = int.TryParse(Console.ReadLine(), out int num);
+    if (isInt)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+        return -1;
+    }
+
+}
