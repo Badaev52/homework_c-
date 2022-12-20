b0973425caf447d2233ad15012604fb6accde8d6
badaev52pk1 <badaev52@gmail.com>
2022-12-20 20:16:12 +0300

    задание 60 решено

--- exercise58/Program.cs
+++ exercise58/Program.cs
@@ -9,3 +9,77 @@
 
 
 
+int row = 3;
+int col = 3;
+int rndMin = 0;
+int rndMax = 100;
+
+int[,] arr1 = Create2dIntArray(row, col, rndMin, rndMax);
+Print2dArray(arr1);
+Console.WriteLine("--------------");
+int[,] arr2 = Create2dIntArray(row, col, rndMin, rndMax);
+Print2dArray(arr2);
+Console.WriteLine("Результат умножения матрицы 1 на матрицу 2:");
+int[,] arr = MatrixMultiplication(arr1, arr2);
+Print2dArray(arr);
+
+
+int[,] MatrixMultiplication(int[,] arr1, int[,] arr2)
+{
+
+    int[,] arr = new int[arr1.GetLength(0), arr1.GetLength(1)];
+
+    for (int i = 0; i < arr1.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr1.GetLength(1); j++)
+        {
+
+            arr[i, j] = arr1[i, j] * arr2[i, j];
+
+        }
+
+
+    }
+
+    return arr;
+}
+
+int[,] Create2dIntArray(int row, int col, int min, int max)
+{
+    Random random = new Random();
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+
+        for (int j = 0; j < col; j++)
+        {
+
+            array[i, j] = random.Next(min, max) + 1;
+
+        }
+
+    }
+
+    return array;
+
+}
+
+void Print2dArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+
+            Console.Write(arr[i, j]);
+            Console.Write('\t');
+
+        }
+        Console.WriteLine();
+
+    }
+
+}
--- exercise60/Program.cs
+++ exercise60/Program.cs
@@ -7,3 +7,65 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+int x = 2;
+int y = 2;
+int z = 2;
+
+int rndMin = 10;
+int rndMax = 99;
+
+int[,,] pointArray = Create3dIntArray(x, y, z, rndMin, rndMax);
+Print3dArray(pointArray);
+
+
+
+int[,,] Create3dIntArray(int x, int y, int z, int rndMin, int rndMax)
+{
+
+    Random random = new Random((int)(DateTime.Now.Ticks));
+    int[,,] array = new int[x, y, z];
+    for (int i = 0; i < x; i++)
+    {
+
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+
+                array[i, j, k] = random.Next(rndMin, rndMax) + 1;
+
+            }
+
+
+        }
+
+    }
+
+    return array;
+
+
+}
+
+void Print3dArray(int[,,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+
+                Console.Write($"{arr[i, j, k]}({i}, {j}, {k})  ");
+
+
+            }
+            Console.WriteLine();
+
+        }
+
+
+    }
+
+}
